c7cf7b8cbca7f7aa9f4838a380d27ef79e859416
dapeventigan <dabeventigan@gmail.com>
2023-11-29 02:02:18 +0800

    modified:   index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://beehubvas.com", "https://beehubvas.onrender.com"],
     methods: ["GET", "POST"],
     credentials: true,
   })
