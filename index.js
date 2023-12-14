a68e756eb570aac13c716895d22bbb881703e8c4
dapeventigan <dabeventigan@gmail.com>
2023-12-14 14:53:50 +0800

    comment out build app.jsx

--- index.js
+++ index.js
@@ -31,12 +31,12 @@ app.use(
 app.use(cookieParser());
 app.use("/resumes", express.static(path.join(__dirname, "resumes")));
 // Serve static files from the build directory (assuming React build files are in 'build' directory)
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
 
-// Catch-all route to serve React app
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'App.jsx'));
-});
+// // Catch-all route to serve React app
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'build', 'App.jsx'));
+// });
 
 mongoose.connect(process.env.DB, {
   useNewUrlParser: true,
