69333c10442b449ab6c7b56e865244db0b9e2c76
dapeventigan <dabeventigan@gmail.com>
2023-12-14 20:12:16 +0800

    minimal change on the build dir

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 // Handle any requests that don't match the ones above
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/build/index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 mongoose.connect(process.env.DB, {
