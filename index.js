3f72504009df924d9f2125ba15af9becef1302e1
dapeventigan <dabeventigan@gmail.com>
2023-12-15 22:49:02 +0800

    updated cors settings v2

--- index.js
+++ index.js
@@ -24,6 +24,12 @@ app.use(
     optionsSuccessStatus: 200,
   })
 );
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
 
 console.log("__dirname:", __dirname);
 
