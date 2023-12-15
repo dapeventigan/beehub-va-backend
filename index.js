8991884cf8d9c20ca3c5e83acd780967cea7e46f
dapeventigan <dabeventigan@gmail.com>
2023-12-15 22:53:18 +0800

    updated cors settings v3 removed wildcard

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(
   })
 );
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://beehubvas.com");
   res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
