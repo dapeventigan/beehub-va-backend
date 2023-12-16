c854c95cba08c6490c8c29cf5e9014e15a9f651d
dapeventigan <dabeventigan@gmail.com>
2023-12-16 22:22:38 +0800

    added set cookie secure to false

--- index.js
+++ index.js
@@ -154,7 +154,7 @@ app.post("/login", async (req, res) => {
       res.cookie("token", token, {
         httpOnly: true,
         domain: "dape-beehub-va-api.onrender.com",
-        secure: true, // Set to true if your application is served over HTTPS
+        secure: false, // Set to true if your application is served over HTTPS
         sameSite: "none",
         path: "/",
         maxAge: 86400000,
